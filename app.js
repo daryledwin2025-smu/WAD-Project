bd53cc5412058ee2e58ca08118e6cfc254b02e1b
Joshua Per <joshuaper2602@gmail.com>
2026-03-16 15:14:32 +0800

    ...

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-// week1 : week1-tools/hello-world-server.js
-
-// Import the built-in 'http module'
-const http = require('http');
-// Define the hostname and port for the server
-const hostname = 'localhost';
-const port = 8000;
-
-
-// Define the server
-const server = http.createServer((req, res) => {
-  // Set the response header to indicate HTML content
-  res.statusCode = 200;
-  res.setHeader('Content-Type','text/html');
-  // Send the "Hello, World!" message to the browser
-  res.end('<h1>Hello, WAD1!</h1>');
-});
-
-server.listen(port, hostname, () => {
- console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-//test
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
 
 
