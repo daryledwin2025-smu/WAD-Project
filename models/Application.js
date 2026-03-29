179218d38ff08f8830d41b3f9d8c436e6a4f5ce4
Joshua Per <joshuaper2602@gmail.com>
2026-03-30 02:50:06 +0800

    .

--- models/Application.js
+++ models/Application.js
@@ -15,6 +15,10 @@ const applicationSchema = new mongoose.Schema({
         type: String, 
         required: true
     },
+    petName: {
+        type: String,
+        required: true
+    },
     livingSituation: {
         type: String,
         required: false // Not required initially so users can save incomplete drafts
