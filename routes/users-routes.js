c852eb1e213c4899d2b7cf190acc2145459e2fe1
dkjw75 <darryl.kek.2025@computing.smu.edu.sg>
2026-03-30 17:42:10 +0800

    added index.html

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const usersController = require("../controllers/users-controller");
 const authMiddleware = require("../middleware/auth");
 
-router.get("/", usersController.displayLogin); 
-router.post("/", usersController.submitLogin);
+router.get("/user-login", usersController.displayLogin); 
+router.post("/user-login", usersController.submitLogin);
 
 router.get("/user-register", usersController.displayRegister);
 router.post("/user-register", usersController.submitRegister);
@@ -17,8 +17,8 @@ router.get('/home', authMiddleware.isLoggedIn, authMiddleware.isAdopter,usersCon
 router.get('/home-shelter', authMiddleware.isLoggedIn, authMiddleware.isShelter, usersController.showHomeShelter);
 
 router.get('/logout', usersController.submitLogout);
-router.get('/index.html', (req, res) => {
-    res.render('user-login'); 
-});
+// router.get('/index.html', (req, res) => {
+//     res.render('user-login'); 
+// });
 
 module.exports = router;
