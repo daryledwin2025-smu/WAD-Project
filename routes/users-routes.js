ef7f9b154952d5fb8bbf9bfc49911a823a698ea5
izz1103 <izzdanish.m.2025@computing.smu.edu.sg>
2026-04-03 01:55:41 +0800

    Clean up

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -9,19 +9,13 @@ router.post("/user-login", usersController.submitLogin);
 router.get("/user-register", usersController.displayRegister);
 router.post("/user-register", usersController.submitRegister);
 
-router.get("/user-edit", authMiddleware.isLoggedIn, usersController.editProfile);
-router.post('/user-edit', authMiddleware.isLoggedIn, usersController.submitEditProfile);
-router.post('/delete-account', authMiddleware.isLoggedIn, usersController.deleteAccount);
-
 router.get('/home', authMiddleware.isLoggedIn, authMiddleware.isAdopter,usersController.showHome);
 router.get('/home-shelter', authMiddleware.isLoggedIn, authMiddleware.isShelter, usersController.showHomeShelter);
 
-router.get('/logout', usersController.submitLogout);
-// router.get('/index.html', (req, res) => {
-//     res.render('user-login'); 
-// });
-
+router.get("/user-edit", authMiddleware.isLoggedIn, usersController.editProfile);
+router.post('/user-edit', authMiddleware.isLoggedIn, usersController.submitEditProfile);
 router.post("/change-password", authMiddleware.isLoggedIn, usersController.changePassword);
-router.post("/edit-profile", authMiddleware.isLoggedIn, usersController.submitEditProfile);
+router.post('/delete-account', authMiddleware.isLoggedIn, usersController.deleteAccount);
+router.get('/logout', usersController.submitLogout);
 
 module.exports = router;
